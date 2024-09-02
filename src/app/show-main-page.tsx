072b435ab6a2a6a213226c09eaee810b0f6b839c
dKurbi <dkurbi@gmail.com>
2024-09-02 18:34:57 +0200

    2nd version, fetching in server side

--- src/app/show-main-page.tsx
+++ src/app/show-main-page.tsx
@@ -9,7 +9,7 @@ export function ShowMainPage({ pokemonData, nextUrl }: { pokemonData: Pokemon[],
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold text-center mb-4">Pokémon List</h1>
-      <ul className="grid grid-cols-3 gap-4">
+      <ul className="grid grid-cols-2 gap-4">
         {pokemon.map((p, index) => (
           <li key={index} className="border p-2 rounded flex items-center">
             <img src={p.image} alt={p.name} className="w-12 h-12 mr-4" />

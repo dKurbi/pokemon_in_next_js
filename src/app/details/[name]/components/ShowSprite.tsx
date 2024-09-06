89fdced07860e689e33702130dcc136e9d4d9039
dKurbi <dkurbi@gmail.com>
2024-09-06 14:33:10 +0200

    show type of sprite not available

--- src/app/details/[name]/components/ShowSprite.tsx
+++ src/app/details/[name]/components/ShowSprite.tsx
@@ -4,6 +4,6 @@ import { ShowSpriteProps } from "@/lib/definitions";
 
 export function ShowSprite({ url, alt, type }: ShowSpriteProps) {
 if (!url)
-	return <div>No ${type} sprite available</div>
+	return <div>No {type} sprite available</div>
 return <img  src={url} alt={alt +" " + type} className="w-48 h-48" />;
 }
